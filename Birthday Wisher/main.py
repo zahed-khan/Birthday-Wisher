75c77e54e85eac40da822570537b1848d3097ca6
zahed-khan <zahedkhan165@gmail.com>
2024-05-21 17:33:23 +0530

    Add files via upload

---
+++ Birthday Wisher/main.py
@@ -0,0 +1,10 @@
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datetime(year=2003, month=1, day=16)
+print(date_of_birth)
